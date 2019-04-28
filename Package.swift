348ca85bc4417db4d734c8e7a0297a81c993a13e
Alex Tran Qui <alex@katalysis.io>
2019-04-28 09:50:53 +0200

    Fix Package.swift for Swift 4.0+

--- Package.swift
+++ Package.swift
@@ -1,6 +1,17 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "ValidationToolkit",
-    exclude: ["Tests"]
+    products: [
+        .library(name: "ValidationToolkit", targets: ["ValidationToolkit"])
+    ],
+    dependencies: [
+     ],
+    targets: [
+    .target(name: "ValidationToolkit", dependencies: [], path: "Sources"),
+        .testTarget(name: "ValidationToolkit.Tests", dependencies: [
+            "ValidationToolkit"], path: "Tests")
+    ]
 )
+
